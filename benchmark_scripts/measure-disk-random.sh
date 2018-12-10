6da3851bb0830f7c89ef867b8d23f9bc910cabc7
CodeLionX <sebastian.schmidl@t-online.de>
2018-12-10 20:08:47 +0100

    adopt disk random script to only output write speed in IOP/s and take around 10s

--- benchmark_scripts/measure-disk-random.sh
+++ benchmark_scripts/measure-disk-random.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # test random read and write operations
-VALUE=$( fio --name=random-rw --rw=randrw --size=1m --directory=/tmp/ --output-format=normal --runtime=18 --time_based | sed -En 's/.*IOPS=([0-9.]+).*/\1/p' | awk '{sum+=$1} END {print sum}')
+VALUE=$( fio --name=random-rw --rw=randrw --size=1m --directory=/tmp/ --output-format=normal --runtime=9 --time_based | sed -En 's/.*avg=([0-9]+).*/\1/p' | tail -n 1)
 rm -f /tmp/random-rw*
 
 printf "%s,%s\n" "$(date +%s)" "$VALUE" # IOPS

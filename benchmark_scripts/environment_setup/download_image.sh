0f1a443d8dc4bb2f710eef839350920954bbc8b9
CodeLionX <sebastian.schmidl@t-online.de>
2018-12-11 15:41:00 +0100

    start and management scripts for qemu and qemu-kvm

---
+++ benchmark_scripts/environment_setup/download_image.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# download and extract hdd image
+curl -o debian_stretch_i386.tar.xz https://www.sec.in.tum.de/~bierbaumer/qemu/debian_stretch_i386_standard.tar.xz
+tar xf debian_stretch_i386.tar.xz debian_stretch_i386_standard/hda.img
+mv debian_stretch_i386_standard/hda.img debian_stretch_i386.img
+rm -r debian_stretch_i386_standard

020ef3745db64f25526f6faa5e6ce5bae72e7ff6
Frederic Schneider <frederic.schneider@student.hpi.uni-potsdam.de>
2018-12-11 15:15:36 +0100

    Use dedicated port mapping for docker-run nginx (8080:80)

--- benchmark_scripts/nginx/build_run_docker.sh
+++ benchmark_scripts/nginx/build_run_docker.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 # only build docker image if it does not exist
-docker build -t nginx-static-serve . >/dev/null
+docker build -t nginx-static-serve .
 
 # make sure the container is not already running
-docker stop nginx &> /dev/null && docker rm nginx &> /dev/null
+docker stop nginx && docker rm nginx
 
 # run the image
-docker run -P -d --name=nginx nginx-static-serve >/dev/null
-
-# grab the connected port accessible to test script
-echo $(docker port nginx | sed 's/.*:\([0-9]*\)/\1/p' | tail -n 1)
+docker run -p 8080:80 -d --name=nginx nginx-static-serve
 
 exit 0

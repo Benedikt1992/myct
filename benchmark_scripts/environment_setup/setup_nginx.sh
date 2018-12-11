e499b2b5cb9b752b3e64c0d4bb36c410fd7df024
CodeLionX <sebastian.schmidl@t-online.de>
2018-12-11 19:06:30 +0100

    improve nginx setup

--- benchmark_scripts/environment_setup/setup_nginx.sh
+++ benchmark_scripts/environment_setup/setup_nginx.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-# check if static serve file has been generated
-if [ ! -f file.dat ]; then
+# ! run script as root user !
+
+# install nginx
+apt-get update
+apt-get install -yq nginx
+apt-get clean
+
+# don't daemonize and disable autostart
+echo "" >> /etc/nginx/nginx.conf
+echo "daemon off;" >> /etc/nginx/nginx.conf
+systemctl disable nginx
+systemctl stop nginx
+
+# static file to serve (must be generated first)
+if [ ! -f docker/file.dat ]; then
     echo "No static serve file 'file.dat' available. Run 'gen_serve_file.sh'!"
     exit 1
 fi
-
-# install nginx
-sudo apt-get update
-sudo apt-get install -yq nginx
-sudo apt-get clean
-
-# static file to serve (must be generated first)
-cp ./docker/file.dat /var/www/html
+cp docker/file.dat /var/www/html/
 
 exit 0

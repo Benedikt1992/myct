3119ae731cd6ce339c66fd536b5e6dad2b0e5218
CodeLionX <sebastian.schmidl@t-online.de>
2018-12-15 16:39:46 +0100

    add uninstall script for fio

---
+++ benchmark_scripts/environment_setup/remove_fio.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm /usr/local/bin/fio 
+rm /usr/local/bin/fio-genzipf
+rm /usr/local/bin/fio-btrace2fio 
+rm /usr/local/bin/fio-dedupe 
+rm /usr/local/bin/fio-verify-state 
+rm /usr/local/bin/fio_generate_plots 
+rm /usr/local/bin/fio2gnuplot 
+rm /usr/local/bin/genfio 
+rm /usr/local/bin/fiologparser.py
+rm /usr/local/bin/fiologparser_hist.py
+rm /usr/local/bin/fio_jsonplus_clat2csv
+rm /usr/local/man/man1/fio.1
+rm /usr/local/man/man1/fio_generate_plots.1
+rm /usr/local/man/man1/fio2gnuplot.1
+rm /usr/local/man/man1/fiologparser_hist.py.1
+rm -d /usr/local/share/fio

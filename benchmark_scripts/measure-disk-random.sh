1393c055124ae091c271b1c9a2692cad065d6614
CodeLionX <sebastian.schmidl@t-online.de>
2018-12-15 17:13:43 +0100

    make measure disk capable of dealing with both fio versions

--- benchmark_scripts/measure-disk-random.sh
+++ benchmark_scripts/measure-disk-random.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # test random read and write operations
-VALUE=$( fio --name=random-rw --rw=randrw --size=1m --directory=/tmp/ --output-format=normal --runtime=9 --time_based | sed -En 's/.*avg=([0-9]+).*/\1/p' | tail -n 1)
+VALUE=$( fio --name=random-rw --rw=randrw --size=1m --directory=/tmp/ --output-format=normal --runtime=9 --time_based )
+fioVersion=$(fio --version)
+
+if [[ ${fioVersion} == "fio-2.16" ]]; then
+	echo "old fio: ${fioVersion}, falling back to old fio output parser" >&2
+	VALUE=$( echo ${VALUE} | sed -En 's/.*iops=([0-9]+).*/\1/p' | tail -n 1 )
+else
+	VALUE=$( echo ${VALUE} | sed -En 's/.*avg=([0-9]+).*/\1/p' | tail -n 1 )
+fi
 rm -f /tmp/random-rw*
 
 printf "%s,%s\n" "$(date +%s)" "$VALUE" # IOPS

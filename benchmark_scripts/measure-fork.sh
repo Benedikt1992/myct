32250ed98efeae94b9d15e085c21d5c2a39d5e3e
CodeLionX <sebastian.schmidl@t-online.de>
2018-12-19 09:06:44 +0100

    measure time that the forksum needs

--- benchmark_scripts/measure-fork.sh
+++ benchmark_scripts/measure-fork.sh
@@ -9,9 +9,10 @@ fi
 
 
 if [ "$SYSTEMROOT" = "C:\Windows" ] ; then
-	result=$(./forksum.exe $ARGS 2>/dev/null)
+	result=$(time (./forksum.exe $ARGS >/dev/null 2>&1) 2>&1)
 else
-	result=$(./${EXECUTABLE} $ARGS 2>/dev/null)
+	result=$(time (./${EXECUTABLE} $ARGS >/dev/null 2>&1) 2>&1)
 fi
+result=$(echo ${result} | grep real | cut -f 2 | awk '{gsub(",", ".", $0); split($0, a, "m|s"); printf("%.3f\n", a[1]*60+a[2])}')
 printf "%s,%s\n" "$(date +%s)" "$result" #sum
 
